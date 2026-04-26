df4245c3391525c3fe894e32351c6bccbaf1f940
tencentcloudapi <tencentcloudapi@tencent.com>
2026-04-27 04:30:11 +0800

    release 3.0.1577

---
+++ src/TencentCloud/Kms/V20190118/Models/RegionQps.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * Copyright (c) 2017-2025 Tencent. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace TencentCloud\Kms\V20190118\Models;
+use TencentCloud\Common\AbstractModel;
+
+/**
+ * 地域的QPS扩展包
+ *
+ * @method string getRegion() 获取<p>地域</p>
+ * @method void setRegion(string $Region) 设置<p>地域</p>
+ * @method integer getQps() 获取<p>qps的大小</p>
+ * @method void setQps(integer $Qps) 设置<p>qps的大小</p>
+ */
+class RegionQps extends AbstractModel
+{
+    /**
+     * @var string <p>地域</p>
+     */
+    public $Region;
+
+    /**
+     * @var integer <p>qps的大小</p>
+     */
+    public $Qps;
+
+    /**
+     * @param string $Region <p>地域</p>
+     * @param integer $Qps <p>qps的大小</p>
+     */
+    function __construct()
+    {
+
+    }
+
+    /**
+     * For internal only. DO NOT USE IT.
+     */
+    public function deserialize($param)
+    {
+        if ($param === null) {
+            return;
+        }
+        if (array_key_exists("Region",$param) and $param["Region"] !== null) {
+            $this->Region = $param["Region"];
+        }
+
+        if (array_key_exists("Qps",$param) and $param["Qps"] !== null) {
+            $this->Qps = $param["Qps"];
+        }
+    }
+}
